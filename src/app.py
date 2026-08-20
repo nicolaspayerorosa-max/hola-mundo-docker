14b92a4778ef49bcb944844145acea43108b7171
Nicolas Alexander Payero Rosa <20251151@itla.edu.do>
2026-08-20 15:22:58 -0400

    Configurar puerto para Render

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
